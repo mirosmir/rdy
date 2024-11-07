8df18dc9c3779ab95ff5bdd20097c9eb94f7aae0
mirosmir <miros.dasha@gmail.com>
2024-11-07 17:59:37 +0300

    Add files via upload

---
+++ anotherfile.py
@@ -0,0 +1,35 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x – y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Ошибка: деление на ноль"
+
+print("Выберите операцию:")
+print("Сложение 1")
+print("Вычитание 2")
+print("Умножение 3")
+print("Деление 4")
+
+choice = input("Введите номер операции (1/2/3/4): ")
+
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+if choice == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif choice == '2':
+    print(f"{num1} – {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:     print("Неверный ввод")
